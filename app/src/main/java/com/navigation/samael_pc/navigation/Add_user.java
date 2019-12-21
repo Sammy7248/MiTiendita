61cb15af223097f7f6161dd8cbb020818195c2e7
Samael <jesuslive1970@gmail.com>
2019-12-20 18:37:25 -0600

    Add method to encrypt user password, add form to add product

--- app/src/main/java/com/navigation/samael_pc/navigation/Add_user.java
+++ app/src/main/java/com/navigation/samael_pc/navigation/Add_user.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,13 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class Add_user extends Fragment {
 
@@ -21,6 +29,7 @@ public class Add_user extends Fragment {
     DatabaseReference child;
     TextInputEditText username, password, nombre, apellido, confirm_password;
     TextView register;
+    public static final String AES = "AES";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,27 +63,41 @@ public class Add_user extends Fragment {
                 if(confirm_password.getText().toString().isEmpty()){
                     confirm_password.setError("Vacio");
                 }
-                if(password.getText().toString().equals(confirm_password.getText().toString())){
-                    String key = general.reference.push().getKey();
-                    Usuario user = new Usuario(username.getText().toString(), password.getText().toString(), nombre.getText().toString(), apellido.getText().toString(), 1, false,key);
-                    //child(key).
-                    child.child(user.getKey()).setValue(user);
-                    username.setText("");
-                    password.setText("");
-                    nombre.setText("");
-                    apellido.setText("");
-                    username.requestFocus();
-                    confirm_password.setText("");
-                    Toast.makeText(v.getContext(), "Usuario registrado correctamente"+key, Toast.LENGTH_LONG).show();
+                if(!username.getText().toString().isEmpty() && !password.getText().toString().isEmpty() && !nombre.getText().toString().isEmpty() && !apellido.getText().toString().isEmpty() && !confirm_password.getText().toString().isEmpty()){
+                    //password.getText().toString().equals(confirm_password.getText().toString())
+                    if(password.getText().toString().equals(confirm_password.getText().toString())){
+                        try {
+                            String key = general.reference.push().getKey();
+                            String pass = null;
+                            pass = encriptar(password.getText().toString());
+                            Usuario user = new Usuario(username.getText().toString(), pass, nombre.getText().toString(), apellido.getText().toString(), 1, false,key);
+                            //child(key).
+                            child.child(user.getKey()).setValue(user);
+                            username.setText("");
+                            password.setText("");
+                            nombre.setText("");
+                            apellido.setText("");
+                            username.requestFocus();
+                            confirm_password.setText("");
+                            Toast.makeText(v.getContext(), "Usuario registrado correctamente"+key, Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    else{
+                        confirm_password.setText("");
+                        Toast.makeText(getContext(), "Contraseñas no son guales, verifique por favor!!!", Toast.LENGTH_LONG).show();
+                    }
                 }
                 else{
-                    Toast.makeText(view.getContext(), "Las contraseñas no son iguales", Toast.LENGTH_LONG).show();
                     username.setText("");
                     password.setText("");
                     nombre.setText("");
                     apellido.setText("");
                     username.requestFocus();
                     confirm_password.setText("");
+                    Toast.makeText(view.getContext(), "Usuario no registrado, campos incorrectos", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -82,5 +105,22 @@ public class Add_user extends Fragment {
 
 
         return view;
+    }
+
+    public static String encriptar(String password) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        keyGenerator.init(128);
+        SecretKey scrKey = keyGenerator.generateKey();
+        byte[] bytesecret = scrKey.getEncoded();
+        SecretKeySpec secSpec = new SecretKeySpec(bytesecret, AES);
+        Cipher cip = Cipher.getInstance(AES);
+        cip.init(Cipher.ENCRYPT_MODE,secSpec);
+        byte[] encryptPass = cip.doFinal(password.getBytes());
+
+        //PARA DESENCRIPTAR EL PASSWORD
+        /*cip.init(Cipher.DECRYPT_MODE,secSpec);
+        byte[] decryptPass = cip.doFinal(encryptPass);*/
+
+        return new String(encryptPass.toString());
     }
 }
--- app/src/main/java/com/navigation/samael_pc/navigation/Productos.java
+++ app/src/main/java/com/navigation/samael_pc/navigation/Productos.java
@@ -1,16 +1,33 @@
 package com.navigation.samael_pc.navigation;
 
+import android.animation.Animator;
+import android.animation.TimeInterpolator;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Interpolator;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class Productos extends Fragment {
@@ -18,12 +35,13 @@ public class Productos extends Fragment {
     RecyclerView recycler_product;
     ProductAdapter adapter;
     ArrayList<Producto> product_list;
+    FloatingActionButton add_prod;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_productos, container, false);
+        final View view = inflater.inflate(R.layout.fragment_productos, container, false);
 
         recycler_product = view.findViewById(R.id.recycler_productos);
         recycler_product.setLayoutManager(new LinearLayoutManager(view.getContext()));
@@ -37,6 +55,69 @@ public class Productos extends Fragment {
 
         adapter = new ProductAdapter(view.getContext(), product_list);
         recycler_product.setAdapter(adapter);
+
+        add_prod = view.findViewById(R.id.add_prod);
+
+        add_prod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder dialog = new AlertDialog.Builder(view.getContext());
+                LayoutInflater inflate = LayoutInflater.from(v.getContext());
+                View view = inflate.inflate(R.layout.add_product,null);
+                dialog.setView(view);
+                dialog.create().show();
+
+                final TextView caducidad = view.findViewById(R.id.caducidad);
+
+                final Spinner area = view.findViewById(R.id.area);
+                ArrayList<String> areas = new ArrayList();
+                areas.add("Abarrotes");
+                areas.add("Papeleria");
+                areas.add("Ropa");
+
+                ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_dropdown_item_1line,areas);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                area.setAdapter(adapter);
+
+                area.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if (area.getSelectedItem().toString().equals("Papeleria") || area.getSelectedItem().toString().equals("Ropa")){
+                            caducidad.setVisibility(View.GONE);
+                        }
+                        else{
+                            caducidad.setVisibility(View.VISIBLE);
+                        }
+
+
+                        caducidad.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Calendar c = Calendar.getInstance();
+                                int day = c.get(Calendar.DAY_OF_MONTH);
+                                int month = c.get(Calendar.MONTH);
+                                int year = c.get(Calendar.YEAR);
+                                DatePickerDialog dpc = new DatePickerDialog(getContext(), R.style.DialogTheme ,new DatePickerDialog.OnDateSetListener() {
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                                        Toast.makeText(getContext(), String.valueOf(year+"/"+month+"/"+day),Toast.LENGTH_LONG).show();
+                                    }
+                                },day,month,year);
+
+                                dpc.show();
+                            }
+
+                        });
+                        //Toast.makeText(getContext(),String.valueOf(area.getSelectedItem().toString()), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+            }
+        });
         return view;
     }
 }
